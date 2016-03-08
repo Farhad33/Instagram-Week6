7304dc449b4ccad75bddefcd89a19c06146cc324
Majid Rahimi <Majid88Rahimi@gmail.com>
2016-03-08 05:15:42 -0800

    Added upload page, table view, login page

---
+++ instagram/Cell.swift
@@ -0,0 +1,24 @@
+//
+//  Cell.swift
+//  instagram
+//
+//  Created by Majid Rahimi on 3/7/16.
+//  Copyright © 2016 Majid Rahimi. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
